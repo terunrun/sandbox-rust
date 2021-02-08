cda2985abdeec16c6b7dd608d4e4c8d5c3cdf77b
terunrun <terunrun@gmail.com>
2021-02-08 22:29:29 +0900

    実行時引数で受け取ったパターンとのマッチングを実装

--- rsgrep/src/main.rs
+++ rsgrep/src/main.rs
@@ -7,6 +7,11 @@ use std::io::{BufReader, BufRead};
 // モジュール全体をインポートすることもできる。
 use std::env;
 
+// regexを宣言して使用できるようにするにはこのように書く。
+extern crate regex;
+// 宣言するだけではダメ。regexからRegex型をインポートする必要がある。
+use regex::Regex;
+
 // TODO:エラーを受け取るようにしたい
 fn usage(s: &String) {
     // println!("引数でファイル名を指定してください。")
@@ -15,7 +20,28 @@ fn usage(s: &String) {
 
 fn main() {
     // envモジュールのargs関数でプログラムの引数を取得できる。
-    // そのうち2番目を`nth`で取得（0番目はプログラムの名前、1番目はパターンで今は無視）。
+    // nth(1)で第一引数からパターンを取得する。
+    let pattern = match env::args().nth(1) {
+        Some(result) => result,
+        None => {
+            usage(&"please specify the patter for matching.".to_string());
+            return;
+        }
+    };
+    usage(&pattern);
+
+    // 取り出したパターンから`Regex`をあらためて作る
+    // 無効な正規表現だった場合などにはエラーが返る
+    let reg = match Regex::new(&pattern) {
+        Ok(result) => result,
+        Err(e) => {
+            println!("invalid regexp {}: {}", pattern, e);
+            return
+        }
+    };
+    usage(&reg.to_string());
+
+    // 第二引数からファイル名を取得する。
     // 引数があるか分からないのでOptionで返される。
     let filename = match env::args().nth(2) {
         // あれば取り出す。
@@ -59,6 +85,13 @@ fn main() {
 
             }
         };
-        println!("{}", line);
+        // println!("{}", line);
+
+        // パターンにマッチしたらプリントする
+        // is_matchはリードオンリーなので参照型を受け取る
+        if reg.is_match(&line) {
+            // 上で参照型で引数に渡したので、ここでも使える
+            println!("{}", line);
+        }
     }
 }
